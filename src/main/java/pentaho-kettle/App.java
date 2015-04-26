a89439bb4d7daa63cd150b65eb89aebd8c0b14b6
Matts Häggblom <matts.haggblom@gmail.com>
2015-04-26 14:09:18 +0200

    Initial commit

---
+++ src/main/java/pentaho-kettle/App.java
@@ -0,0 +1,13 @@
+package pentaho-kettle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
